9d6be91fd6fdab652007e1c2b68a06057f43e812
tono2002 <110565791+tono2002@users.noreply.github.com>
2022-08-17 14:22:39 +0200

    Update myFirstBot.py

--- myFirstBot.py
+++ myFirstBot.py
@@ -1,17 +1,18 @@
 import time
 import pyautogui
 
+# NOTA: Los valores de la posición x e y no son iguales para todos los dispositivos
 
 print("BIENVENIDO AL PASADOR DE HISTORIAS")
 print("-----------------------------------")
 print("Iniciando...")
 
-time.sleep(8) #Tiempo para poner instagram en pantalla completa
+time.sleep(8) # Tiempo para poner instagram en pantalla completa
 
-pyautogui.moveTo(1361, 680, 1)
+pyautogui.moveTo(1361, 680, 1) # Ratón se mueve a la posición donde se encuentra el botón de pasar historia (>)
 time.sleep(2)
 
-n = 200
+n = 200 # Número de historias que se desean pasar
 while n > 0:
     pyautogui.click()
     n = n - 1
